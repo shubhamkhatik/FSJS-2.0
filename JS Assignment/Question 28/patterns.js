c8485179e69273f95e2cc94d70818ca884d67354
shubham khatik <40956270+shubhamkhatik@users.noreply.github.com>
2023-03-04 12:04:18 +0530

    js ass done

---
+++ JS Assignment/Question 28/patterns.js
@@ -0,0 +1,27 @@
+// a. Print a triangle pattern, if the given input is 3 then the pattern should be similar to the given output
+
+function triangle(n) {
+  for (let i = 0; i < 3; i++) {
+    console.log("*".repeat(n + i - 2));
+  }
+}
+triangle(3);
+
+// b. Print a square pattern, if the input is 3 then the output should be similar to the given output
+
+function square(n) {
+  for (let i = 0; i < n; i++) {
+    console.log("*".repeat(n));
+  }
+}
+square(3);
+
+// c. Print a pyramid pattern, if the input is 3 then the output should be similar to the given output
+
+function pyramid(n) {
+  for (let i = 0; i < n; i++) {
+    console.log(" ".repeat(n - i - 1) + "*".repeat(1 + 2 * i));
+  }
+}
+
+pyramid(3);
